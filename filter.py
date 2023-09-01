3923318f91b1405b9f3afa787b2214962fb5938e
Poyan <100019286+poyans@users.noreply.github.com>
2023-09-01 12:18:51 +0330

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,335 @@
+# Poyan Teror
+# chanel Rubika poyan_teror
+print("")
+print("")
+print(" ")
+print("""
+      
+ஜ۩۞۩ஜஜ۩۞۩ஜஜ۩۞۩ஜ
+█▄█ █ █▀█ █▄█ █▀█ █▀█
+█▀█ █ █▀▀ █▀█ █▄█ █▀▀
+ஜ۩۞۩ஜஜ۩۞۩ஜஜ۩۞۩ஜ
+
+                            """)
+print(" ")
+print("""
+
+[▁ ▂ ▄ ▅ ▆ ▇ █ Loding... █ ▇ ▆ ▅ ▄ ▂ ▁]
+      
+                            """)
+print (" ")
+print("Load . . .")                 
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# colors
+red='\033[31m'
+orange='\033[33m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("      GOD FILTERING")
+print (Fore.YELLOW + "")
+x = """ Loading ~ ~ ~ 
+
+🛑 🛑
+🛑        🛑
+🛑        🛑
+  🛑🛑 🛑
+            🛑
+ 🛑      🛑
+   🛑  🛑
+
+
+
+     🛑🛑🛑
+  🛑        🛑
+🛑          🛑
+  🛑        🛑
+     🛑🛑🛑
+                 🛑
+
+
+🛑       🛑
+🛑  🛑     🛑
+🛑🛑
+🛑
+🛑
+🛑
+🛑
+
+
+
+🛑🛑🛑
+  🛑      🛑
+  🛑        🛑
+  🛑        🛑
+  🛑        🛑
+  🛑        🛑
+
+
+
+     🛑🛑🛑
+  🛑        🛑
+🛑          🛑
+  🛑        🛑
+     🛑🛑🛑
+                 🛑
+
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+print(f"{orange} ")
+d = ("""
+
+[ıllıllı salar lovin Poyan Teror  ıllıllı]
+
+                
+     
+[▁ ▂ ▄ ▅ ▆ ▇ █ Loding... █ ▇ ▆ ▅ ▄ ▂ ▁]
+
+     
+⠀""")
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+# Chaker * _ *  Hal Kardi?
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("start - - - ! ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       RUBIKA ID - @poyan_teror")
+print("                      RUBIKA= On -- @yadeegri")
+print("            eta salar= @sali_Begafil")
+                            
+print ("                telegram @Godalah")
+print ("instagram @poyan.salahian")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("!!")
+time.sleep(0.7)
+print ("loding....")
+time.sleep(0.7)
+print ("y/n")
+time.sleep(0.6)
+print ("....")
+time.sleep(0.5)
+print ("......")
+time.sleep(0.4)
+print (".........")
+time.sleep(0.3)
+print ("..................")
+time.sleep(0.2)
+print (".........................")
+time.sleep(0.1)
+print (".............................")
+time.sleep(0.5)
+print ("...............................")
+time.sleep(0.1)
+print (".................................")
+time.sleep(0.5)
+print ("....................................")
+time.sleep(0.1)
+print ("~iegn")
+time.sleep(0.5)
+print ("cood info server")
+time.sleep(0.1)
+print ("script server cood")
+time.sleep(0.5)
+print ("server1:y//d/f")
+time.sleep(1.0)
+print ("server2:f//d/")
+time.sleep(0.5)
+print ("server3:g/d//")
+time.sleep(0.1)
+print ("server4:d//f/h")
+time.sleep(0.5)
+print ("server5:f//a/y")
+time.sleep(1.0)
+print ("poyan  script")
+time.sleep(0.5)
+print ("script poyan cood filter rubika.apk 3.0.0 80%")
+time.sleep(1.0)
+print ("script server")
+time.sleep(0.5)
+print ("script connect server rubika.ir/rubika.com/ file os server")
+time.sleep(1.0)
+print ("cood filter group 90% apn cood proxy-server")
+time.sleep(0.5)
+print ("cood filter account rubika 87% apn cood filter proxy-port:8000")
+time.sleep(1.0)
+print ("cood filter chaneel rubika 78% apn proxy file port:auth")
+time.sleep(0.5)
+print ("")
+print (" ")
+print ("")
+# 1 chanel 2 Group 3 acc
+up = input(""" 
+
+[cood filter channel rubika (1) ]
+[cood filtr group rubika    (2) ]
+                 
+[cood filter Account rubika (3) ]
+                  
+[spamer   (0) ]
+
+[Exit    (4) ]
+
+[report aut (5) ]
+
+[Code Rp (6) ]
+
+[ account on filter (7) ]
+
+Which do you want>>>""")
+print("")
+print(f"{pink} ")
+time.sleep(3.0)
+print(""" in the name of God 
+_____________
+
+Loading . . . .
+
+. . . . . . .  . .
+
+ . . . . . . . . . . 
+ 
+💣 💣 💣 💣 💣 💣 💣 💣
+
+                    
+    
+|||||Telegram: T.me/godalah ||||
+|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|
+____________ id rubika:poyan_teror ___________
+""")
+print(f"{red} ")
+
+if up == "7":
+    os.system("python sms.py")
+if up == "6":
+    os.system("python report.py")
+if up == "5":
+    os.system("python pass.py")
+if up == "4":
+    os.system("exit 5")
+    
+if up == "0":
+         print ('(spamer) Code filter =>' + '%s%s  (/http://spam.ir/yftt15k-XXX.090001/law-purn.ir.4.8.5.8/g//d/9.8.9.6.5.4.3.8.6.5.2.5.5.4.2.5.9.2.7.8.4.1.3.1.6.9.9.1.2.4.6.9.4.7.2.9.1.(D.a.r.k.)) %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+if up == "1":
+
+        print ('code Fil Channel ≠>' + '%s%s (7.8.9.2.d/f///y//6065///g.k.4.6.7.8.2.3.5.6.7.3.2.9.3.9.1.5.3.6.2.9.6/) %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "1":
+        print("    COD  ")
+
+
+if up == "2":
+        print ('code group ≠>' + '%s%s (4.0.1.4/f.i.d/filter/group/**fakes/h.o.r.s.j.l.o.o.o.w.qgl/4.5.3//5.2./w/p/fil-rubika/hakers-fil-fil./4/5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.0.1.3) %s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "2":
+
+        print("     cσ∂s")
+
+
+if up == "3":
+
+        print ('code Filter account ≠>' + '%s%s (http://2.4.yftt15k.0.1.4.0.d//f/h.7.5.7.8Y.9.2.5.yftt_07.6.8.3.3.y//d/.9.2f///g.h(#XXX)./e/f///g.h////./f/g.h/XXX//4.2.1././/CCC/g//CCC/h.e//pnp//f.g/.e///.fg/.3(#filtreng).y/.d.f.6.0.3.2.7.0.2.5.3.8.6.g.d//.4.2.3.0.4=(https://mossad.gov.il)=(http://mi6.rozblog.com)=spam.ir/yftt15k-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.@SupportBot(http://vxvault.net//ViriList.php)(/00/xxx*9//report.account.rubika/0//9//0/0//0//1*7*6*6*6/law-purn.ir<AF+.AF.AF.Fil/XXX/00/0/0/0/01/1/3/4/yftt15k/5/2/2/5/6/7//5/6/7/0/0/2/4/5/3/6/6/6/4/3/2/5*/) %s' % (fg('red'), bg('blue'), attr('reset')))
+
+if up != "3":1
+print("""
+        
+             lider golder""")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""poyan and salar""")
+print (" chanel rubikam =>> https://rubika.ir/yadeegri ")
+print(f"{blue}" )
+print(" ")
+print("love hack")
+print("")
+print(" im poyan teror")
+print("")
+print("ip rubika.ir = 5.106.10.226 ")
+print("")
+print(" id Telegram=>> @godalah -")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Ostoreh Filtering-hacker Poyan Teror and salar ")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Cod Ro Dorost Bezan 70% Filter Mikone  ' + '%s%s TanK You My Friend  %s'  % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print (""" {code ro ( 5 ) bar bezanin to Sayer!!}
+baraye account Tanks""")
+print(" ")
+print(" ")
+print(" ")
+print ("          THE END!")
+print("")
+print(""" (Update Kardan script) 👇:
+
+rm -rf filter-rubika
+rm -rmrf filter.py
+
+
+git clone https://github.com/poyans/poyan-teror
+      
+
+chmod +x Filter.py
+
+
+cd poyan-teror
+
+python filter.py
+
+""")
+print("")
+print("")
+print("         </teror>")
+print ("")
+time.sleep(0.3)
+print(f"{red}" )
+time.sleep(1)
+print ("""ostorh majazi""")
+print("")
+print (Fore.WHITE + "")
